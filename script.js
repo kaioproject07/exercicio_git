6c59d0e0f053555a6f92d854675954420c228fa5
kaioproject07 <kaio777csos@gmail.com>
2025-05-29 20:51:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 Num sei o que sei que la
+Eu sou o mc poze to aqui d novo
+vim fazer uma homenagem pra tropa do rodo
+pros meus irmaozin, que hoje ta ceu
+que hoje tá no ceu, que na favela do rodo fez o seu papel
